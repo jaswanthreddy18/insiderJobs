0de2323535a92224f9df0df3ecab5b15a30ed1c7
jaswanthreddy18 <145342070+jaswanthreddy18@users.noreply.github.com>
2025-05-31 16:44:30 +0530

    Update constatnt.js

--- frontend/src/components/utils/constatnt.js
+++ frontend/src/components/utils/constatnt.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "https://insider-jobs-flax.vercel.app/api/v1/user";
-export const JOB_API_END_POINT = "https://insider-jobs-flax.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://insider-jobs-flax.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT = "https://insider-jobs-flax.vercel.app/api/v1/company";
+export const USER_API_END_POINT = "https://insiderjobs-dgkc.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://insiderjobs-dgkc.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://insiderjobs-dgkc.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://insiderjobs-dgkc.onrender.com/api/v1/company";
 
