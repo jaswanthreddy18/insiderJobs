edf84d116fda57883664fe01dba914cc39f38269
jaswanthreddy18 <145342070+jaswanthreddy18@users.noreply.github.com>
2025-05-31 17:00:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,8 @@ dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-const corsOption = {
-    origin: [
-    "http://localhost:5173",
-    "https://insider-jobs-phx7.vercel.app"
-    ],
-    credentials : true
-}   
-app.use(cors(corsOption));
+app.use(cookieParser());  
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute)
