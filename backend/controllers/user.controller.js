c633fc0831643fb10d4fe0315d2440328fa66356
jaswanthreddy18 <145342070+jaswanthreddy18@users.noreply.github.com>
2025-05-31 16:59:31 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -89,11 +89,18 @@ export const login = async (req, res) => {
             role:user.role,
             profile:user.profile
         }
-        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000, httpsOnly:true, sameSite:'strict'}).json({
-            message:`Welcome back ${user.fullname}`,
-            user,
-            success:true
-        })
+       res.cookie("token", token, {
+          maxAge: 24 * 60 * 60 * 1000, // 1 day
+          httpOnly: true,
+          secure: true,                // ensures it's only sent over HTTPS
+          sameSite: 'none'             // allows cross-origin cookie usage
+        });
+        
+        return res.status(200).json({
+          message: `Welcome back ${user.fullname}`,
+          user,
+          success: true
+        });
     } catch (error) {
         console.log(error);
     }
