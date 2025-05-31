b51ab4bfe0024922921a0fb8a958f05878568b4b
jaswanthreddy18 <145342070+jaswanthreddy18@users.noreply.github.com>
2025-05-31 12:49:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin : 'http://localhost:5173',
+    origin: [
+    "http://localhost:5173",
+    "https://insider-jobs-phx7.vercel.app"
+    ],
     credentials : true
 }   
 app.use(cors(corsOption));
